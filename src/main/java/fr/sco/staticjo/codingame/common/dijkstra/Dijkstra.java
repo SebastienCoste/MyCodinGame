a486a26e6835b5bbb39ca88f8f7864868cf05cdd
Sebastien Coste <teddy.coste@gmail.com>
2017-01-18 20:12:11 +0100

    improvments

--- src/main/java/fr/sco/staticjo/codingame/common/dijkstra/Dijkstra.java
+++ src/main/java/fr/sco/staticjo/codingame/common/dijkstra/Dijkstra.java
@@ -2,52 +2,60 @@ package fr.sco.staticjo.codingame.common.dijkstra;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import fr.sco.staticjo.codingame.common.dijkstra.Dijkstra.Tree.Node;
 
-	/**
-	 *  Work In Progress 
-	 *  
-	 * @author Static
-	 *
-	 * Common class used to find the lightest path in a tree 
-	 */
+/**
+ *  Work In Progress 
+ *  
+ * @author Static
+ *
+ * Common class used to find the lightest path in a tree 
+ */
 public class Dijkstra{
 
 	public class DijkstraException extends Exception {}
-	
+
 	public interface LeafInter {
-		
+
 		long getWeightToTheNextStep();
 		long getEstimatedWeightToTheEnd(LeafInter end);
 		List<LeafInter>getAllMatchingNeighbours(LeafInter[][] map);
 		boolean isTheEnd(LeafInter end);
+		void seen(boolean hasBeenSeen);
+		boolean canBeTreated(); //For example if you don't want to treat an alrealy seen leaf
 	}
-	
+
 	public static class Tree<T extends LeafInter> {
-	    private Node<T> root;
+		private Node<T> root;
 
-	    public Tree(T rootData, T end) {
-	        root = new Node<T>(rootData, end);
-	        root.data = rootData;
-	        root.children = new ArrayList<Node<T>>();
-	    }
+		public Tree(T rootData, T end) {
+			root = new Node<T>(rootData, end);
+			root.data = rootData;
+			root.children = new ArrayList<Node<T>>();
+		}
 
-	    public static class Node<T extends LeafInter> implements Comparable {
-	    	public long currentWeight;
+		public static class Node<T extends LeafInter> implements Comparable {
+			@Override
+			public String toString() {
+				return "Node [data=" + data + "]";
+			}
+
+			public long currentWeight;
 			public long estimatedWeightLeft;
-	        private T data;
-	        private Node<T> parent;
-	        private List<Node<T>> children;
-	        
-	        public Node(T data, T end){
+			private T data;
+			private Node<T> parent;
+			private List<Node<T>> children;
+
+			public Node(T data, T end){
 				this.data = data;
 				currentWeight = 0;
 				estimatedWeightLeft = data.getEstimatedWeightToTheEnd(end);
 			}
-			
+
 			public Node(Node<T> parent, long weightFromparent, T data, T end){
 				this.data = data;
 				this.parent = parent;
@@ -61,14 +69,14 @@ public class Dijkstra{
 				return Long.compare(currentWeight + estimatedWeightLeft, test.currentWeight + test.estimatedWeightLeft);
 			}
 
-	    }
+		}
 	}
-	
+
 	public LeafInter[][] map;
 	public Tree<LeafInter> tree;
 	public LeafInter end;
 	public List<Node<LeafInter>> leavesToAnalyse;
-	
+
 	public Dijkstra(LeafInter[][] map, LeafInter start, LeafInter end) throws DijkstraException{
 		if (!end.isTheEnd(end)){
 			throw new DijkstraException();
@@ -79,16 +87,33 @@ public class Dijkstra{
 		leavesToAnalyse = new ArrayList<>();
 		leavesToAnalyse.add(tree.root);
 	}
-	
+
 	public List<LeafInter> buildTheTreeForThePathToTheEnd(){
-		
+
 		List<LeafInter> path = new ArrayList<>();
 		Node<LeafInter> bestLeaf = tree.root;
-		
-		while (! bestLeaf.data.isTheEnd(end)){
-			leavesToAnalyse.remove(0);
-			insertInLeavesToAnalyse(getChildrenAsLeaves(bestLeaf));
-			bestLeaf = leavesToAnalyse.get(0);
+
+		while (!bestLeaf.data.isTheEnd(end)){
+			Iterator<Node<LeafInter>> it = leavesToAnalyse.iterator();
+			while(it.hasNext() && it.next().data.canBeTreated()){
+				it.remove();
+			}
+			if (leavesToAnalyse.size() == 0){
+				bestLeaf = cutTheTree(bestLeaf);
+			} else {
+				bestLeaf = leavesToAnalyse.get(0);
+			}
+			if(!bestLeaf.data.isTheEnd(end)){
+				leavesToAnalyse.remove(0);
+				bestLeaf.data.seen(true);
+				List<Node<LeafInter>> childrenAsLeaves = getChildrenAsLeaves(bestLeaf);
+				if (childrenAsLeaves != null && childrenAsLeaves.size() > 0){
+					insertInLeavesToAnalyse(childrenAsLeaves);
+					bestLeaf = leavesToAnalyse.get(0);
+				} else {
+					bestLeaf = cutTheTree(bestLeaf);
+				}
+			}
 		}
 
 		while (bestLeaf.parent != null){
@@ -99,20 +124,37 @@ public class Dijkstra{
 		return path;
 	}
 
-	private List<Node<LeafInter>> getChildrenAsLeaves(Node<LeafInter> bestLeaf) {
-		return bestLeaf.data.getAllMatchingNeighbours(map)
-				.stream()
-				.map(e -> new Node<LeafInter>(bestLeaf, bestLeaf.currentWeight, e, end))
-				.collect(Collectors.toList());
+	private Node<LeafInter> cutTheTree(Node<LeafInter> bestLeaf){
+		while (bestLeaf.parent.children.size() == 1){
+			bestLeaf = bestLeaf.parent;
+		}
+		bestLeaf.parent.children.remove(bestLeaf);
+		bestLeaf = bestLeaf.parent;
+		bestLeaf.data.seen(false);
+		insertInLeavesToAnalyse(bestLeaf);
+		return leavesToAnalyse.get(0);
 	}
-	
+	private List<Node<LeafInter>> getChildrenAsLeaves(Node<LeafInter> bestLeaf) {
+		if (bestLeaf.children == null || bestLeaf.children.size() == 0){
+			bestLeaf.children = bestLeaf.data.getAllMatchingNeighbours(map)
+					.stream()
+					.map(e -> new Node<LeafInter>(bestLeaf, bestLeaf.currentWeight, e, end))
+					.collect(Collectors.toList());
+		}
+		return bestLeaf.children;
+	}
+
 	private void insertInLeavesToAnalyse(List<Node<LeafInter>> nodes){
 		leavesToAnalyse.addAll(nodes);
 		Collections.sort(leavesToAnalyse);
 	}
-	
-	
-	
+	private void insertInLeavesToAnalyse(Node<LeafInter> node){
+		leavesToAnalyse.add(node);
+		Collections.sort(leavesToAnalyse);
+	}
+
+
+
 }
 
 
